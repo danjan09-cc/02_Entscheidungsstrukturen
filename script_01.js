1b9f433f90b3f3744084d42d403aa7d77b67ed5c
Peter <schmidt.multimedia@gmail.com>
2021-07-29 14:33:13 +0200

    05 Fallunterscheidung / CASE|SWITCH 1

--- script_01.js
+++ script_01.js
@@ -13,8 +13,8 @@ isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
 
 // Ausgabe
-console.log("isJohnOlder: " + isJohnOlder);
-console.log("isJohnEqual: " + isJohnEqual);
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
 
 /************ IF  ************/
 // TINA --> There is no alternative!
@@ -49,20 +49,52 @@ console.log("isJohnEqual: " + isJohnEqual);
 /************ IF - ELSE IF  ************/
 // mit alternativen Fällen (älter,jünger,gleich alt)
 
-if (isJohnOlder) 
-// Ja-Zweig / true
-{
-    console.log("John ist älter.");
-}
-// 1. Alternative
-else if(isJohnEqual)
-{
-    console.log("John ist gleich alt.");
-}
-// ggf. weitere Alternativen ..
+// if (isJohnOlder) 
+// // Ja-Zweig / true
+// {
+//     console.log("John ist älter.");
+// }
+// // 1. Alternative
+// else if(isJohnEqual)
+// {
+//     console.log("John ist gleich alt.");
+// }
+// // ggf. weitere Alternativen ..
 
-// Nein-Zweig / false
-else 
+// // Nein-Zweig / false
+// else 
+// {
+//     console.log("John ist jünger.");
+// }
+
+/****** Fallunterscheidung / CASE|SWITCH 1 ******/
+
+const firstName = "Jane";
+let job;
+job = "driver";  // .. fährt TAXI!
+job = "diver"; // .. taucht im Rhein!
+job = "artist"; // .. malt ein Bild!
+job = "pilot"; // .. macht etwas anderes!
+
+job = "teacher"; // .. unterrichtet!
+job = "instructor"; // .. unterrichtet!
+
+switch (job) 
 {
-    console.log("John ist jünger.");
+    case "driver": //1. Fall (case)
+        console.log(firstName + " fährt TAXI!");
+        break;
+    case "diver": //2. Fall (case)
+        console.log(firstName + " taucht im Rhein!");
+        break;
+    case "artist": //3. Fall (case)
+        console.log(firstName + " malt ein Bild!");
+        break;
+    case "teacher": //4. Fall (case)
+    case "instructor": //4a. Fall (case)
+        console.log(firstName + " unterrichtet!");
+        break;
+    default: // für alle nicht bezeichneten Fälle ... WICHTIG !!!!
+        console.log(firstName + " macht etwas anderes!");
+        break;
 }
